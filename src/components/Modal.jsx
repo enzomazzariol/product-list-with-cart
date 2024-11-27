5f7869258ebfecc4d8e97e2e5b510ef0b6a9e060
mazzariolenzo <mazzariolenzo@gmail.com>
2024-11-27 21:37:36 +0100

    fix cantidades del cart y modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,41 +1,38 @@
 /* eslint-disable react/prop-types */
-// src/components/Modal.jsx
-import '../css/App.css';
-
 const Modal = ({ showModal, closeModal, totalPrice, cart }) => {
-  if (!showModal) return null;  // Si no se debe mostrar el modal, no renderiza nada.
+  if (!showModal) return null;
+
+  const itemsInModal = cart.filter((item) => item.cantidad > 0);
 
   const handleClickOutside = (e) => {
-    // Cerrar el modal si se hace clic en el fondo
     if (e.target.classList.contains('modal-overlay')) {
       closeModal();
     }
   };
 
-
   return (
     <div className="modal-overlay" onClick={handleClickOutside}>
       <div className="modal-container d-flex flex-column p-4" onClick={(e) => e.stopPropagation()}>
-        <img src="src\assets\images\icon-order-confirmed.svg" alt="icon confirm order" className='modal-icon mb-3' />
-        <h2 className='modal-title'>Order Confirmed</h2>
-        <p className='modal-description'>We hope you enjoy your food!</p>
-        <div className='order-list-container p-4 mb-4'>
-          {cart.map((item) => (
-            <section key={item.id} className='order-item-container d-flex justify-content-between align-items-center pb-2 mb-3'>
-              <div className='d-flex'>
-                <img src={`src/${item.image.thumbnail}`} alt="image thumbnail dessert" className='order-item-img' />
-                <div className='d-flex flex-column ms-3'>
-                  <p className='cart-item-name mb-2'>{item.name}</p>
-                  <div className='d-flex'>
+        <img src="src/assets/images/icon-order-confirmed.svg" alt="icon confirm order" className="modal-icon mb-3" />
+        <h2 className="modal-title">Order Confirmed</h2>
+        <p className="modal-description">We hope you enjoy your food!</p>
+        <div className="order-list-container p-4 mb-4">
+          {itemsInModal.map((item) => (
+            <section key={item.id} className="order-item-container d-flex justify-content-between align-items-center pb-2 mb-3">
+              <div className="d-flex">
+                <img src={`src/${item.image.thumbnail}`} alt="image thumbnail dessert" className="order-item-img" />
+                <div className="d-flex flex-column ms-3">
+                  <p className="cart-item-name mb-2">{item.name}</p>
+                  <div className="d-flex">
                     <p className="cart-item-cantidad me-3">{item.cantidad}x</p>
                     <p className="cart-item-price">
-                        <span className="cart-arroba">@</span>
-                        ${item.price.toFixed(2)}
+                      <span className="cart-arroba">@</span>
+                      ${item.price.toFixed(2)}
                     </p>
                   </div>
                 </div>
               </div>
-              <p className='cart-item-price order'>${item.price.toFixed(2)}</p>
+              <p className="cart-item-price order">${item.price.toFixed(2)}</p>
             </section>
           ))}
           <div className="d-flex justify-content-between align-items-center mt-4">
@@ -43,7 +40,7 @@ const Modal = ({ showModal, closeModal, totalPrice, cart }) => {
             <p className="cart-total">${totalPrice.toFixed(2)}</p>
           </div>
         </div>
-        <button className='confirm-order-button p-2'>Start new Order</button>
+        <button className="confirm-order-button p-2">Start new Order</button>
       </div>
     </div>
   );
--- src/components/dessertCard.jsx
+++ src/components/dessertCard.jsx
@@ -1,94 +1,86 @@
 import { useState, useEffect } from "react";
 /* eslint-disable react/prop-types */
 
-export const DessertCard = ({dessert, addToCart}) => {
-    const [cardSelected, setCardSelected] = useState(false);
+const DessertCard = ({ dessert, addToCart }) => {
     const [imageUrl, setImageUrl] = useState(`src/${dessert.image.desktop}`);
-
-    if (!dessert) {
-        return <p>No dessert data available</p>;
-    }
-
+  
     useEffect(() => {
-        const updateImageUrl = () => {
-            if (window.innerWidth < 768) {
-                setImageUrl(`src/${dessert.image.mobile}`); // Cambia a la versión móvil
-            } else {
-                setImageUrl(`src/${dessert.image.desktop}`); // Cambia a la versión desktop
-            }
-        };
-
-        updateImageUrl(); // Llamar al inicio para establecer el valor inicial
-        window.addEventListener("resize", updateImageUrl); // Escuchar cambios en el tamaño
-
-        // Limpiar el evento al desmontar el componente
-        return () => {
-            window.removeEventListener("resize", updateImageUrl);
-        };
-    }, [dessert.image.desktop, dessert.image.mobile]);
-
-    // Función para agregar el postre al carrito con la cantidad
-    const handleAddToCart = () => {
-        setCardSelected(true); 
-        addToCart(dessert, 1); 
-    };
-
-    // Función para incrementar la cantidad
-    const incrementarCantidad = (e) => {
-        e.stopPropagation();  // evitar que se propague el evento al padre
-        addToCart(dessert, 1); 
-    };
-
-    // Función para decrementar la cantidad
-    const decrementarCantidad = (e) => {
-        e.stopPropagation(); // evitar que se propague el evento al padre
-        if (dessert.cantidad > 1) {
-            addToCart(dessert, -1); 
+      const updateImageUrl = () => {
+        if (window.innerWidth < 768) {
+          setImageUrl(`src/${dessert.image.mobile}`);
         } else {
-            setCardSelected(false); 
-            addToCart(dessert, -1);
+          setImageUrl(`src/${dessert.image.desktop}`);
         }
+      };
+  
+      updateImageUrl();
+      window.addEventListener('resize', updateImageUrl);
+  
+      return () => {
+        window.removeEventListener('resize', updateImageUrl);
+      };
+    }, [dessert.image.desktop, dessert.image.mobile]);
+  
+    const handleAddToCart = () => {
+      addToCart(dessert, 1);
     };
-
+  
+    const incrementarCantidad = (e) => {
+      e.stopPropagation();
+      addToCart(dessert, 1);
+    };
+  
+    const decrementarCantidad = (e) => {
+      e.stopPropagation();
+      addToCart(dessert, -1);
+    };
+  
     return (
-        <div className='dessert-card d-flex flex-column'> 
-            <div className='d-flex flex-column justify-content-center align-items-center'>
-                <img src={imageUrl} alt="image of dessert" className={`dessert-card-img ${cardSelected ? 'selected' : ''}`} />
-                <button 
-                    className={`${cardSelected ? 'selected-button d-flex align-items-center justify-content-between p-2' : 'dessert-card-button p-2'}`}
-                    onClick={handleAddToCart}
-                >
-                    {cardSelected || dessert.cantidad > 0 ? (
-                        <>
-                            <button className="button-incrementar" onClick={decrementarCantidad}>
-                                <img 
-                                    src="src/assets/images/icon-decrement-quantity.svg" 
-                                    alt="decrement icon"
-                                    className="decrement-icon"
-                                />
-                            </button>
-                            <span className="cantidad-card">{dessert.cantidad}</span> 
-                            <button className="button-incrementar" onClick={incrementarCantidad} >
-                                <img 
-                                    src="src/assets/images/icon-increment-quantity.svg" 
-                                    alt="increment icon"
-                                    className="increment-icon"
-                                />
-                            </button>
-                        </>
-                    ) : (
-                        <>
-                            <img src="src/assets/images/icon-add-to-cart.svg" alt="add to cart icon" className='dessert-card-icon me-2 mb-1' />
-                            Add to cart
-                        </>
-                    )}
-                </button>
+      <div className="dessert-card d-flex flex-column">
+        <div className="d-flex flex-column justify-content-center align-items-center">
+          <img
+            src={imageUrl}
+            alt="image of dessert"
+            className="dessert-card-img"
+          />
+          {dessert.cantidad > 0 ? (
+            <div className="selected-button d-flex align-items-center justify-content-between p-2">
+              <button className="button-incrementar" onClick={decrementarCantidad}>
+                <img
+                  src="src/assets/images/icon-decrement-quantity.svg"
+                  alt="decrement icon"
+                  className="decrement-icon"
+                />
+              </button>
+              <span className="cantidad-card">{dessert.cantidad}</span>
+              <button className="button-incrementar" onClick={incrementarCantidad}>
+                <img
+                  src="src/assets/images/icon-increment-quantity.svg"
+                  alt="increment icon"
+                  className="increment-icon"
+                />
+              </button>
             </div>
-            <small className='dessert-card-name'>{dessert.name}</small>
-            <strong className='dessert-card-description'>{dessert.category}</strong>
-            <p className='dessert-card-price'>${dessert.price}</p>
+          ) : (
+            <button
+              className="dessert-card-button p-2"
+              onClick={handleAddToCart}
+            >
+              <img
+                src="src/assets/images/icon-add-to-cart.svg"
+                alt="add to cart icon"
+                className="dessert-card-icon me-2 mb-1"
+              />
+              Add to cart
+            </button>
+          )}
         </div>
+        <small className="dessert-card-name">{dessert.name}</small>
+        <strong className="dessert-card-description">{dessert.category}</strong>
+        <p className="dessert-card-price">${dessert.price}</p>
+      </div>
     );
-};
-
-export default DessertCard;
+  };
+  
+  export default DessertCard;
+  
